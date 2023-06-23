5a0b2e6bb05cb738a2a4d64c98b5df0710c92761
kinhoz <kinroshima2@gmail.com>
2023-06-23 16:35:06 -0300

    produto

--- trocar_img_referencia.js
+++ trocar_img_referencia.js
@@ -3,17 +3,18 @@ const referenceImages = document.querySelectorAll('.reference-image');
 
 // Percorre todas as imagens de referência
 referenceImages.forEach((image) => {
-  // Verifica se a imagem de referência possui um caminho definido
   const referenceImage = image.querySelector('img');
   const referenceImagePath = referenceImage.getAttribute('src');
-  const imageExists = new Image();
-  imageExists.src = referenceImagePath;
-  imageExists.onerror = function () {
-    // Se o caminho da imagem de referência não for reconhecido, oculta a div
-    image.style.display = 'none';
-  };
 
-  if (referenceImagePath && referenceImagePath !== 'img/' && imageExists.complete) {
+  if (referenceImagePath && referenceImagePath !== 'img/') {
+    // Verifica se a imagem de referência é válida
+    const imageExists = new Image();
+    imageExists.src = referenceImagePath;
+    imageExists.addEventListener('error', () => {
+      // Se a imagem de referência não for encontrada, oculta a div
+      image.style.display = 'none';
+    });
+
     // Adiciona o evento de clique na imagem de referência
     image.addEventListener('click', () => {
       // Remove a classe 'active' de todas as imagens de referência
@@ -28,7 +29,7 @@ referenceImages.forEach((image) => {
       const productImage = document.querySelector('.product-image img');
 
       // Atualiza o atributo src da imagem do produto com o caminho da imagem de referência
-      productImage.setAttribute('src', referenceImagePath);
+      productImage.src = referenceImagePath;
     });
   }
 });
